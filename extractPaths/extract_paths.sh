6daaa59cab6237ad903c44c1c20855ae64467cb1
Hector Puch <hectorpuchortonoves@gmail.com>
2025-09-04 09:55:07 +0200

    Initial commit

---
+++ extractPaths/extract_paths.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "Uso: $0 <URL>"
+    exit 1
+fi
+
+URL=$1
+DOMAIN=$(echo "$URL" | sed -E 's~https?://~~; s~/.*~~')
+
+echo "[+] Extracting unique paths from: $DOMAIN"
+
+curl -s "$URL" \
+| grep -oP "https?://$DOMAIN(/[a-zA-Z0-9._/?=&%-]*)" \
+| sed -E "s~https?://$DOMAIN~~" \
+| sort -u
